3bba31abc480bc660a9ecdbe4b70e1fa656ad02e
Dan Bikle <danbikle@outlook.com>
2019-01-16 21:32:01 -0800

    more

--- test/system/get_copper_test.rb
+++ test/system/get_copper_test.rb
@@ -9,7 +9,21 @@ class GetCopperTest < ApplicationSystemTestCase
   test "visiting investing.com" do
     Capybara.run_server = false
     Capybara.app_host   = 'https://www.investing.com'
+    sleep 2
     visit '/'
+    sleep 2
+    # ScreenshotHelper.take_screenshot
+    take_screenshot
+    sleep 2
+    # Kernel.system
+    system 'mv tmp/screenshots/test_visiting_investing.com.png /tmp/img1.png'
+    sleep 2
+    visit '/commodities/'
+    sleep 2
+    take_screenshot
+    sleep 2
+    system 'mv tmp/screenshots/test_visiting_investing.com.png /tmp/img2.png'
+    sleep 2
     assert true
     #    assert_selector "h1", text: "investing.com"
   end
