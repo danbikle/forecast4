1b5be7010dabdd057c9b1ba7f2e1ce095712575a
Dan Bikle <danbikle@outlook.com>
2019-01-18 00:51:03 -0800

    more

--- test/system/get_copper_test.rb
+++ test/system/get_copper_test.rb
@@ -1,34 +1,25 @@
 # get_copper_test.rb
 
+# This script should get historical copper prices.
+
 # Demo:
 # rails test test/system/get_copper_test.rb
 
 require "application_system_test_case"
 
 class GetCopperTest < ApplicationSystemTestCase
+  # For me, :headless_chrome fails:
+  # driven_by :selenium, using: :headless_chrome
   test "visiting investing.com" do
     Capybara.run_server = false
     Capybara.app_host   = 'https://www.investing.com'
     sleep 2
-#   visit '/'
-#   sleep 2
-#   # ScreenshotHelper.take_screenshot
-#   take_screenshot
-#   sleep 2
-#   # Kernel.system
-#   system 'mv tmp/screenshots/test_visiting_investing.com.png /tmp/img1.png'
-#   sleep 2
     visit '/commodities/'
     sleep 2
-    take_screenshot
-    sleep 2
-    system 'mv tmp/screenshots/test_visiting_investing.com.png /tmp/img2.png'
-    sleep 2
     find('#userAccount > div > a.login.bold').click
-    sleep 4
+    sleep 3
     ENV['INVUSR']
     ENV['INVPSS']
-    popup_elm = find("#loginPopupform")
     email_elm = find("#loginFormUser_email")
     pass_elm  = find("#loginForm_password")
     email_elm.fill_in with: ENV['INVUSR']
@@ -38,8 +29,6 @@ class GetCopperTest < ApplicationSystemTestCase
     buttn_elm = find("#signup > a.newButton.orange")
     sleep 2
     buttn_elm.click
-    sleep 2
-    visit '/commodities/copper'
     sleep 2
     visit '/commodities/copper-historical-data'
     sleep 2
